01979dca1dc6128e83c2b25163ee006e110d87a6
chelinomx <107780203+chelinomx@users.noreply.github.com>
2023-10-14 23:11:44 -0500

    Update settings.js

--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -149,7 +149,6 @@ function themeChange(ele) {
             if (!inFrame && !navigator.userAgent.includes("Firefox")) {
                 const popup = open("about:blank", "_blank");
                 if (!popup || popup.closed) {
-                    alert("Please allow popups and redirects.");
                 } else {
                     const doc = popup.document;
                     const iframe = doc.createElement("iframe");
